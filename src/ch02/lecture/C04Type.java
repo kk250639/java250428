0f7632c7c80100e8394b1fc6c5ae9c916bfcbe10
kk250639 <chesyeo@gmail.com>
2025-04-28 14:40:51 +0900

    java 자료형

--- src/ch02/lecture/C04Type.java
+++ src/ch02/lecture/C04Type.java
@@ -6,12 +6,16 @@ public class C04Type {
         // 기본타입 (primitive type)
         // .정수형
         // byte, short, int, long
+
         // .실수형
         //  float, double
+
         // .논리형
         //  boolean
+
         // .문자형
         // char
+        
         // 참조타입 (reference type)
         // 나머지다
     }

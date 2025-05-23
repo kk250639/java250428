8cab1e531adb9c423c50aafe277656964cbe58bc
kk250639 <chesyeo@gmail.com>
2025-05-23 17:47:12 +0900

    복습하기 위해 삭제 & 재작성

--- src/ch13/lecture/p01/App1.java
+++ src/ch13/lecture/p01/App1.java
@@ -1,4 +1,21 @@
 package ch13.lecture.p01;
 
 public class App1 {
+    public static void main(String[] args) {
+        Box01 box = new Box01();
+//        box.setItem(99);
+//        Integer v = box.getItem();
+//        System.out.println("v = " + v);
+
+        box.setItem(3.14);  //
+    }
+}
+class Box01 {
+    private Double item;
+    public Double getItem() {
+        return item;
+    }
+    public void setItem(Double item) {
+        this.item = item;
+    }
 }
--- src/메모.java
+++ src/메모.java
@@ -2,6 +2,7 @@ public class 메모 {
     /* 파라미터
        method 기능 방식
        App04의 출력 방식
+       double Double
     */
 
 }
